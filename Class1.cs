5928a47dad4580950ca83fb9e31cedee33884541
dposen <dposen@L320-52-230>
2019-11-24 17:19:35 +0200

    1.3

--- Class1.cs
+++ Class1.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.HostingUnit;
 
 namespace dotNet5780_02_1840_9920
 {
     class Host
     {
         public int HostKey;
+        private <List>HostingUnit HostingUnitCollection;
+        public Host(int _HostKey, <List>HostingUnit _HostingUnitCollection, int size)
+        {
+            HostKey = _HostKey;
 
+        }
+        private SetHostingUnitCollection()
+        {
+
+        }
+        public GetHostingUnitCollection()
+        {
+
+        }
     }
 }
