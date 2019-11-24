8b08e68efd4d70853e88eb128b9505ea10252d96
matanya finkelstein <matanya54@gmail.com>
2019-11-24 15:17:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_02_1840_9920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
