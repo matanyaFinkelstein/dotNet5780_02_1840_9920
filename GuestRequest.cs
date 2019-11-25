24b754ecdd6c5e7f3267a6e1e9c0c7699bd828b6
dposen <dposen@L320-52-230>
2019-11-25 19:07:03 +0200

    1.5

--- GuestRequest.cs
+++ GuestRequest.cs
@@ -8,44 +8,20 @@ namespace dotNet5780_02_1840_9920
 {
     class GuestRequest
     {
-        private string EntryDate;
-        private string ReleaseDate;
-        private bool IsApproved;
+        public string EntryDate;
+        public string ReleaseDate;
+        public bool IsApproved;
         public GuestRequest()
         {
             EntryDate = null;
             ReleaseDate = null;
             IsApproved = false;
         }
-        public void SetEntryDate(string _EntryDate)
-        {
-            EntryDate = _EntryDate;
-        }
-        public void SetReleaseDate(string _ReleaseDate)
-        {
-            ReleaseDate = _ReleaseDate;
-        }
-        public void SetIsApproved(bool _IsApproved)
-        {
-            IsApproved = _IsApproved;
-        }
-        public string GetEntryDate()
-        {
-            return EntryDate;
-        }
-        public string GetReleaseDate()
-        {
-            return ReleaseDate;
-        }
-        public bool GetIsApproved()
-        {
-            return IsApproved;
-        }
         public void ToString()
         {
-            Console.WriteLine("The entry date: {0}", GetEntryDate());
-            Console.WriteLine("The release date: {0}", GetReleaseDate());
-            if (GetIsApproved())
+            Console.WriteLine("The entry date: {0}", EntryDate);
+            Console.WriteLine("The release date: {0}", ReleaseDate);
+            if (IsApproved)
                 Console.WriteLine("The request was accepted");
             else
                 Console.WriteLine("The request was rejected");
